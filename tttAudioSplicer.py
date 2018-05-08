4d16467d3fbd0f0878a7fc0d95528a30377e4399
Drew Alexander <22423862@student.uwa.edu.au>
2018-05-08 20:37:59 +0800

    restaged file

--- tttAudioSplicer.py
+++ tttAudioSplicer.py
@@ -1,10 +1,32 @@
-def main():
-    
+from pydub import AudioSegment
+from pydub.utils import make_chunks
+from glob import glob
 
-def open_file(filename):
-    if os.path.isfile(filename):
-        inFile = open(filename, 'r')
-        return inFile
-    else:
-        print("Cannot locate file {0}".format(filename))
+
+def main():
+    epNum = str(input("Enter episode number: "))
+    audio, text = getEpisode(epNum)
+    print(audio, text)
+
+
+def getEpisode(epNum):
+   audioPath = glob('Audio/'+epNum+'*.wav')
+   textPath = glob('Text/'+epNum+'*.*')
+   return audioPath, textPath
+
+def spliceAudio(fileName, fileType):
+    if fileName == []:
+        print("Could not locate audio file")
         return None
+    audioFile = AudioSegment.from_file(fileName, fileType) 
+    clip_length_ms = 1000 # pydub calculates in millisec
+    clips = make_chunks(audioFile, clip_length_ms) #Make chunks of one sec
+    clips[0].export("testClip", format=fileType)
+    #exportChuncks(chunks)
+    
+def exportClips(clips, fileType): 
+    #Export all of the individual chunks as mp3 files
+    for i, clip in enumerate(clips):
+        clip_name = "{0}Clip.{1}".format(i, fileType)
+        print("exporting", clip_name)
+        clip.export(clip_name, format=fileType)
