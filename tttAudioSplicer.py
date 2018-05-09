042954418f6a9e4b89fca49c2aacb31a838c91d6
Drew Alexander <22423862@student.uwa.edu.au>
2018-05-09 22:12:50 +0800

    added toMilli, getTimes and fixed ffmpeg issues

--- tttAudioSplicer.py
+++ tttAudioSplicer.py
@@ -1,28 +1,49 @@
 from pydub import AudioSegment
 from pydub.utils import make_chunks
 from glob import glob
-
+import pysrt
 
 def main():
     epNum = str(input("Enter episode number: "))
-    audio, text = getEpisode(epNum)
-    print(audio, text)
-
+    audioFile, textFile = getEpisode(epNum)
+    print(audioFile, textFile)
 
 def getEpisode(epNum):
-   audioPath = glob('Audio/'+epNum+'*.wav')
-   textPath = glob('Text/'+epNum+'*.*')
+   audioPath = glob('Audio/'+str(epNum)+'*.*')
+   if audioPath == []:
+       print("Could not locate audio")
+       return None
+   textPath = glob('Text/'+str(epNum)+'*.*')
+   if textPath == []:
+       print("Could not locate text")
+       return None
    return audioPath, textPath
 
-def spliceAudio(fileName, fileType):
+def toMilli(time):
+    timeMilli = time.hours*3600000
+    timeMilli += time.minutes*60000
+    timeMilli += time.seconds*1000
+    timeMilli += time.milliseconds
+    return timeMilli
+    
+def getTimes(textFile, index):
+    text = pysrt.open(textFile)
+    timeStart = toMilli(text[index].start)
+    timeEnd = toMilli(text[index].end)
+    time = [timeStart, timeEnd]
+    print(text[index].text)
+    return time
+
+    
+def spliceAudio(fileName, fileType, time):
     if fileName == []:
         print("Could not locate audio file")
         return None
-    audioFile = AudioSegment.from_file(fileName, fileType) 
-    clip_length_ms = 1000 # pydub calculates in millisec
-    clips = make_chunks(audioFile, clip_length_ms) #Make chunks of one sec
-    clips[0].export("testClip", format=fileType)
-    #exportChuncks(chunks)
+    AudioSegment.converter = "./ffmpeg"
+    audioFile = AudioSegment.from_file(fileName, fileType)
+    clip = audioFile[time[0]:time[1]]
+    clip.export("testClip", format=fileType)
+    #exportClips(clips, mp3)
     
 def exportClips(clips, fileType): 
     #Export all of the individual chunks as mp3 files
