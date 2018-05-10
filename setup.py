88f03201a253d19e1147183f06fd795870fce724
Drew Alexander <22423862@student.uwa.edu.au>
2018-05-10 22:13:15 +0800

    Added setup framework

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
-from setuptools import setup
+from subprocess import call
 
-setup (
-    name = 'tttAudioSplicer',
-    version = '1.0',
-    author = 'Drew Alexander',
-    url = 'https://github.com/DrewAlexander98/TtT-Transcript-Splicer.git',
-    py_modules= ['tttAudioSplicer']
-    install_requires=[
-        'pydub',
-        'glob'
-    )
+call(['ls', '-l'])
